4b6f6b6cd260f6485403e40f156d351227e670a2
Hanif Carroll <HanifCarroll@gmail.com>
2020-10-30 15:59:08 -0400

    Add header.

---
+++ src/components/header/index.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import styles from "./styles.module.scss";
+
+export const Header = props => {
+  const [searchValue, setSearchValue ] = useState('');
+  return (
+      <header className={styles.header}>
+        <h1 className={styles.logo}>Notes</h1>
+        <input
+            className={styles.search}
+            type="search"
+            placeholder="Search notes..."
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+        />
+      </header>
+  );
+};
